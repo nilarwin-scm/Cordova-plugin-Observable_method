bcbe99ebf6b5071f2aebf05d203a75795f4b5644
Nilar_Win <scm.nilarwin@gmail.com>
2020-05-22 09:16:58 +0630

    carry data

--- src/ios/Observable_method.swift
+++ src/ios/Observable_method.swift
@@ -1,8 +1,9 @@
 @objc(Observable_method) class Observable_method : CDVPlugin {
   @objc(Hello_World:) 
   func Hello_World(command: CDVInvokedUrlCommand) { 
+    var data = command.argument(at: 0)as! String
         var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: data);
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
 }
--- www/Observable_method.js
+++ www/Observable_method.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = "Observable_method"; 
 
 var Observable_method = function() {}; 
-Observable_method.Hello_World = function(onSuccess, onError) {
-    exec(onSuccess, onError, PLUGIN_NAME, "Hello_World", []);
+Observable_method.Hello_World = function(data,onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "Hello_World", [data]);
  };
 module.exports = Observable_method;
