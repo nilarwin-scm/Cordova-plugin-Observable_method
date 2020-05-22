156946e4ca8f6ad2c55d5076c1c09389e9c35138
Nilar_Win <scm.nilarwin@gmail.com>
2020-05-22 16:30:35 +0630

    change .swift file

--- src/ios/Observable_method.swift
+++ src/ios/Observable_method.swift
@@ -1,15 +1,15 @@
 @objc(Observable_method) class Observable_method : CDVPlugin {
-  @objc(Hello_World:) 
+  @objc(Hello_World:)
   func Hello_World(command: CDVInvokedUrlCommand) {
     print("count world\(command.argument(at: 0))")
     let text = command.argument(at: 0) as! String
     let pluginResult:CDVPluginResult
-    print("count data",echo)
-    if text != nil && text!.count > 0 {
-        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: text!)
+    if text != nil && text.count > 0 {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: text)
     } else {
         pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
     }
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
 }
+
