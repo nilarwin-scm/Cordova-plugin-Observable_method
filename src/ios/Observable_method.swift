f0fdccd0f93ddd3d61938555700637cd405a2ea7
Nilar_Win <scm.nilarwin@gmail.com>
2020-05-22 13:33:51 +0630

    update carry data

--- src/ios/Observable_method.swift
+++ src/ios/Observable_method.swift
@@ -1,9 +1,9 @@
 @objc(Observable_method) class Observable_method : CDVPlugin {
   @objc(Hello_World:) 
   func Hello_World(command: CDVInvokedUrlCommand) { 
-    var data = command.argument(at: 0)as! String
+    var sMessage = command.argument(at: 0)as! String
         var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: data);
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: sMessage);
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
   }
 }
