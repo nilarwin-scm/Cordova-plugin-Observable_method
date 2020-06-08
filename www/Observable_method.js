f10cecbc16fc2ee7b568977d572838c4a6d1e0aa
Nilar_Win <scm.nilarwin@gmail.com>
2020-06-08 14:41:54 +0630

    update js file

--- www/Observable_method.js
+++ www/Observable_method.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = "Observable_method"; 
 
 var Observable_method = function() {}; 
-Observable_method.Hello_World = function(HelloWorldOptions, success, error) {
-    exec(success, error, PLUGIN_NAME, "Hello_World", [HelloWorldOptions.sMessage]);
+Observable_method.Hello_World = function(options, success, error) {
+    exec(success, error, PLUGIN_NAME, "Hello_World", [options.sMessage]);
  };
 module.exports = Observable_method;
