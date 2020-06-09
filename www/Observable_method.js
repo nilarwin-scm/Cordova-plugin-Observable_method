5903b3fc81b7910830d7d50d1d15342e1a63a60b
Nilar_Win <scm.nilarwin@gmail.com>
2020-06-09 09:57:51 +0630

    update js file

--- www/Observable_method.js
+++ www/Observable_method.js
@@ -4,18 +4,18 @@ var PLUGIN_NAME = "Observable_method";
 
 var Observable_method = function() {}; 
 Observable_method.Hello_World = function(options, success, error) {
+  if(!error){
+    options.sMessage = "options.sMessage1"
+   error = function(){};
+  }
+  if (options.sMessage == null) {
+      options.sMessage= "options.sMessage null"
+    error("Null msg isn't supported");
+    return;
+  }
     exec(success, error, PLUGIN_NAME, "Hello_World", [options.sMessage]);
  };
  Observable_method.carrycolor = function(color, onSuccess, onError) {
-    //if error is null then replace with empty function to silence warnings
-    if(!error){
-       error = function(){};
-     }
-    
-     if (color === null) {
-       error("Null msg isn't supported");
-       return;
-     }
-       exec(onSuccess, onError, PLUGIN_NAME, "carrycolor", [color]);
-     };
+    exec(onSuccess, onError, PLUGIN_NAME, "carrycolor", [color]);
+  };
 module.exports = Observable_method;
